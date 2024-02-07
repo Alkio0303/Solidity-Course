b04c4f602a141bc731407b5a6f883ec32719251b
Alkio0303 <103888198+Alkio0303@users.noreply.github.com>
2024-02-07 22:04:09 +0200

    Update FundMe.sol
    Edited the functions

--- Lesson 4 - FundMe/FundMe.sol
+++ Lesson 4 - FundMe/FundMe.sol
@@ -1,15 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe{
-    
-    uint256 public minimalUsd = 5;
-    
+    using PriceConverter for uint256;
+
+    uint256 public minimalUsd = 5e18;
+
+    address[] public funders;
+    mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
+
     function fund() public payable{
-        require(msg.value > minimalUsd, "Did not send enough ETH!");
+        require(msg.value.getConversionRate() >= minimalUsd, "Did not send enough ETH!");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
     function withdraw() public{
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+           address funder = funders[funderIndex];
+           addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+
+        //Transfer
+        payable(msg.sender).transfer(address(this).balance);
+
+        //Send
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send Failed");
+
+        //Call
+        (bool callSuccess,) =payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed");
 
     }
 }
